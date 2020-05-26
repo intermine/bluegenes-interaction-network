2b0ffd7f7908d82c0b9d44b5baaa51d11b4500e1
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-26 15:55:27 +0530

    updated rootContainer by calling queryData with params passed for demo purpose to get the graph data

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { queryData } from './query';
+import GeneInteractionNetwork from './components/GeneInteractionNetwork';
 
-class RootContainer extends React.Component {
-	render() {
-		return (
-			<div className="rootContainer">
-				<h1>Your Data Viz Here</h1>
-			</div>
-		);
-	}
-}
+const RootContainer = ({ serviceUrl, entity }) => {
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		queryData({
+			serviceUrl: serviceUrl,
+			geneId: entity.value
+		}).then(data => {
+			setData(data);
+		});
+	}, []);
+	return (
+		<div className="rootContainer">
+			{data.length ? (
+				<GeneInteractionNetwork data={data} />
+			) : (
+				<h1>Loading...</h1>
+			)}
+		</div>
+	);
+};
 
 export default RootContainer;
---
+++ src/components/GeneInteractionNetwork.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+
+function GeneInteractionNetwork({ data }) {
+	useEffect(() => {}, [data]);
+
+	return <div id="cy"></div>;
+}
+
+export default GeneInteractionNetwork;
