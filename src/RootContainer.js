da7a625f684b8118314b19e756e1ab6a8762d569
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-29 22:46:33 +0530

    created interaction detail side panel containing info about the selected node

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,29 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { queryData } from './query';
 import GeneInteractionNetwork from './components/GeneInteractionNetwork';
+import InteractionDetail from './components/InteractionDetail';
 
 const RootContainer = ({ serviceUrl, entity }) => {
 	const [data, setData] = useState([]);
+	const [loading, setloading] = useState(false);
+	const [selectedNodeData, setSelectedNodeData] = useState({});
+
 	useEffect(() => {
+		setloading(true);
 		queryData({
 			serviceUrl: serviceUrl,
 			geneId: entity.value
 		}).then(data => {
+			setloading(false);
 			setData(data);
 		});
 	}, []);
+
+	const getSelectedNodeData = node => {
+		const { data, edges } = node.target[0]._private;
+		const { target } = edges[0]._private.data;
+		data.parent = target;
+		setSelectedNodeData(data);
+	};
+
 	return (
 		<div className="rootContainer">
-			<div className="innerContainer">
-				<div className="graph">
-					<span className="chart-title">Gene Interaction Network</span>
-					{data.length ? (
-						<GeneInteractionNetwork data={data} />
-					) : (
-						<h1>Loading...</h1>
-					)}
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className="innerContainer">
+					<div className="graph">
+						<span className="chart-title">Interaction Network</span>
+						{data.length ? (
+							<GeneInteractionNetwork
+								data={data}
+								sendNodeData={getSelectedNodeData}
+							/>
+						) : (
+							<h2>Data Not Found!</h2>
+						)}
+					</div>
+					<div className="controls">
+						<InteractionDetail nodeData={selectedNodeData} />
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
--- src/components/GeneInteractionNetwork.js
+++ src/components/GeneInteractionNetwork.js
@@ -5,12 +5,16 @@ import { createCytoscapeConfig, getGraphData } from '../utils';
 
 cytoscape.use(coseBilkent);
 
-function GeneInteractionNetwork({ data }) {
+const GeneInteractionNetwork = ({ data, sendNodeData }) => {
 	useEffect(() => {
-		cytoscape(createCytoscapeConfig(getGraphData(data || [])));
+		let cy = cytoscape(createCytoscapeConfig(getGraphData(data || [])));
+		let node = cy.elements().nodes();
+		node.bind('tap', event => {
+			sendNodeData(event);
+		});
 	}, [data]);
 
 	return <div id="cy" className="cyContainer"></div>;
-}
+};
 
 export default GeneInteractionNetwork;
