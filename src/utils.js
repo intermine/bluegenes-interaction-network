97ba2d56b922af79339b29b49ea54a2d8580a170
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-30 17:25:57 +0530

    Added haystack edge layout to show multiple color for multiple interaction type. Separated data according to type.

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,71 @@
-function getGraphData(data) {
+function getGraphData(info) {
+	const d = separateDataAccToType(info);
 	const elements = [];
-	data.forEach(el => {
-		const { symbol, primaryIdentifier } = el;
-		elements.push({
-			group: 'nodes',
-			data: {
-				id: el.symbol,
-				info: {
-					class: el.class,
-					symbol,
-					primaryIdentifier
+	d.forEach(data => {
+		data.forEach(el => {
+			const { symbol, primaryIdentifier } = el;
+			elements.push({
+				group: 'nodes',
+				data: {
+					id: el.symbol,
+					bg: '#6582F9',
+					info: {
+						class: el.class,
+						symbol,
+						primaryIdentifier
+					}
 				}
-			}
-		});
-		el.interactions &&
-			el.interactions.forEach(interactors => {
-				const { symbol, primaryIdentifier } = interactors.participant2;
-				elements.push({
-					group: 'nodes',
-					data: {
-						id: symbol,
-						info: {
-							class: interactors.class,
-							primaryIdentifier
-						}
-					}
-				});
-				elements.push({
-					group: 'edges',
-					data: {
-						target: el.symbol,
-						source: symbol
-					}
-				});
 			});
+			el.interactions &&
+				el.interactions.forEach(interactors => {
+					const { symbol, primaryIdentifier } = interactors.participant2;
+					const { type } = interactors.details[0];
+					elements.push({
+						group: 'nodes',
+						data: {
+							id: symbol,
+							bg: '#F9E465',
+							info: {
+								class: interactors.class,
+								primaryIdentifier,
+								details: interactors.details
+							}
+						}
+					});
+					elements.push({
+						group: 'edges',
+						data: {
+							target: el.symbol,
+							source: symbol,
+							group: type == 'physical' ? 'phy' : 'gen'
+						}
+					});
+				});
+		});
 	});
 	return elements;
+}
+
+function separateDataAccToType(data) {
+	let phy = [],
+		gen = [];
+	data.forEach(d => {
+		let p = false,
+			g = false;
+		d.interactions.forEach(i => {
+			i.details.forEach(e => {
+				if (!p && e.type == 'physical') {
+					phy.push(d);
+					p = true;
+				}
+				if (!g && e.type == 'genetic') {
+					gen.push(d);
+					g = true;
+				}
+			});
+		});
+	});
+	return [phy, gen];
 }
 
 function createCytoscapeConfig(elements) {
@@ -47,13 +77,22 @@ function createCytoscapeConfig(elements) {
 			{
 				selector: 'node',
 				style: {
-					label: 'data(id)'
+					label: 'data(id)',
+					'background-color': 'data(bg)'
 				}
 			},
 			{
 				selector: 'edge',
 				style: {
-					'line-color': '#ccc'
+					'line-color': 'green',
+					'curve-style': 'haystack'
+				}
+			},
+			{
+				selector: 'edge[group=gen]',
+				style: {
+					'line-color': 'red',
+					'haystack-radius': '0.6'
 				}
 			}
 		],
