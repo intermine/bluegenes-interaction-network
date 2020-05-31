b4f28466eb1f5d3d69d5259c5a20b65282a9a692
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-31 12:19:06 +0530

    fixed interaction detail of parent node

--- src/components/InteractionDetail.js
+++ src/components/InteractionDetail.js
@@ -16,49 +16,57 @@ const InteractionDetail = ({ nodeData }) => {
 					<div className="title">
 						Symbol: <strong>{nodeData.id}</strong>
 					</div>
-					<div className="title">Interaction Details:</div>
-					<div className="interaction-container">
-						{nodeData.info.details.map(d => (
-							<div className="gene-container" key={d}>
-								<div className="gene-title">{d.name}</div>
-								<div className="gene-detail">
-									<div>
-										Type: <strong>{d.type}</strong>
-									</div>
-									<div>
-										Roles:
-										<div className="role">
-											{d.role1 == 'unspecified role' ||
-											d.role2 == 'unspecified role' ? (
-												<div>	
-													Unspecified Role: <strong>{nodeData.parent}</strong>
+					{nodeData &&
+						nodeData.info &&
+						nodeData.info.details &&
+						nodeData.info.details.length && (
+							<>
+								<div className="title">Interaction Details:</div>
+								<div className="interaction-container">
+									{nodeData.info.details.map(d => (
+										<div className="gene-container" key={d}>
+											<div className="gene-title">{d.name}</div>
+											<div className="gene-detail">
+												<div>
+													Type: <strong>{d.type}</strong>
 												</div>
-												) : (
-												<>
-													<div>
-														{d.role1}: <strong>{nodeData.id}</strong>
+												<div>
+													Roles:
+													<div className="role">
+														{d.role1 == 'unspecified role' ||
+														d.role2 == 'unspecified role' ? (
+															<div>
+																Unspecified·Role:{'·'}
+																<strong>{nodeData.parent}</strong>
+															</div>
+														) : (
+															<>
+																<div>
+																	{d.role1}: <strong>{nodeData.id}</strong>
+																</div>
+																<div>
+																	{d.role2}: <strong>{nodeData.parent}</strong>
+																</div>
+															</>
+														)}
 													</div>
-													<div>
-														{d.role2}: <strong>{nodeData.parent}</strong>
-													</div>
-												</>
-											)}
-										</div>
-									</div>
-									{d.dataSets.length && (
-										<div>
-											DataSet:
-											{d.dataSets.map(data => (
-												<div className="data-set" key={data}>
-													{data.name}
 												</div>
-											))}
+												{d.dataSets.length && (
+													<div>
+														DataSet:
+														{d.dataSets.map(data => (
+															<div className="data-set" key={data}>
+																{data.name}
+															</div>
+														))}
+													</div>
+												)}
+											</div>
 										</div>
-									)}
+									))}
 								</div>
-							</div>
-						))}
-					</div>
+							</>
+						)}
 				</div>
 			)}
 		</div>
