ea65a644073bd7dc0d1a14f0e336250b7998105c
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-03 15:22:38 +0530

    added test to query data with correct and false inputs

---
+++ tests/query.test.js
@@ -0,0 +1,33 @@
+import imjs from 'imjs';
+import { queryData } from '../src/query';
+
+describe('query', () => {
+	const mockData = {
+		entity: [5268, 128],
+		service: 'https://www.humanmine.org/humanmine'
+	};
+
+	test('should return a promise resolving with correct data', () => {
+		const promise = queryData({
+			geneId: mockData.entity,
+			serviceUrl: mockData.service,
+			imjsClient: imjs
+		}).catch(() => {});
+
+		expect(promise).resolves.toBeInstanceOf(Array);
+		return promise.then(res => {
+			expect(res.length).toBeGreaterThanOrEqual(1);
+			expect(res[0].symbol).not.toBeFalsy();
+			expect(res[0].interactions.length).toBeGreaterThanOrEqual(0);
+		});
+	});
+
+	test('should return a rejected promise when data not available', () => {
+		const promise = queryData({
+			geneId: 'SOME-FAKE-LIST-NAME',
+			serviceUrl: mockData.service,
+			imjsClient: imjs
+		});
+		return promise.catch(res => expect(res).toBe('No data found!'));
+	});
+});
